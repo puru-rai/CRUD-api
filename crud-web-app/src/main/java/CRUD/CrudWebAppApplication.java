9e9dc68b4c8b8c1567400234c54d561b2c0fec50
puru-rai <raipuru99@gmail.com>
2020-07-22 15:17:02 +0530

    CRUD api progm added

---
+++ crud-web-app/src/main/java/CRUD/CrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWebAppApplication.class, args);
+	}
+
+}
